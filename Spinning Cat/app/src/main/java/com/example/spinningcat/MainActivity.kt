6c21ced1579416285961222d2b957127c618dcdf
Yeray <yeray.fermosomo@elorrieta-errekamari.com>
2025-10-16 00:13:58 +0200

    Enlaces de los Terminos de servicio y politica de privacidad funcionales (y alguna sorpresita jiji)

--- Spinning Cat/app/src/main/java/com/example/spinningcat/MainActivity.kt
+++ Spinning Cat/app/src/main/java/com/example/spinningcat/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.spinningcat
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +28,12 @@ class MainActivity : AppCompatActivity() {
 
         @Suppress("DEPRECATION")
         window.navigationBarColor = getColor(R.color.bgColor) // Cambia el color de la barra de navegación
+
+        // the TextView from the layout file
+        val textView = findViewById<TextView>(R.id.TOS_PP)
+
+        // Finding and displaying the content
+        // that consists a URL as a hyperlink
+        textView.movementMethod = LinkMovementMethod.getInstance()
     }
 }

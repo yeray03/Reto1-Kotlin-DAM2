1582f993e0f96c17b613dfb75c414a1e31e62bb9
Yeray <yeray.fermosomo@elorrieta-errekamari.com>
2025-10-17 18:43:11 +0200

    añadido icono a la aplicacion, renombradas las actividades "Cliente"->"Client" y "Entrenador" -> "Trainer"

--- Spinning Cat/app/src/main/java/com/example/spinningcat/activities/Client.kt
+++ Spinning Cat/app/src/main/java/com/example/spinningcat/activities/Client.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.spinningcat.R
 
-class Entrenador : AppCompatActivity() {
+class Client : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_entrenador)
+        setContentView(R.layout.activity_cliente)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- Spinning Cat/app/src/main/java/com/example/spinningcat/activities/Trainer.kt
+++ Spinning Cat/app/src/main/java/com/example/spinningcat/activities/Trainer.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.spinningcat.R
 
-class Cliente : AppCompatActivity() {
+class Trainer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cliente)
+        setContentView(R.layout.activity_entrenador)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
